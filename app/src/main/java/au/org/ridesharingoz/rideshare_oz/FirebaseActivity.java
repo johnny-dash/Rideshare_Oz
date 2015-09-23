fb05307d4c260fecad9fc8286cc8760d210fc709
Daniel Chiuchiarelli <DChiuch@gmail.com>
2015-09-24 01:21:01 +1000

    Removed useless menus, moved some authenticated checks from base class to authenticated class, started work on maps activity

--- app/src/main/java/au/org/ridesharingoz/rideshare_oz/FirebaseActivity.java
+++ app/src/main/java/au/org/ridesharingoz/rideshare_oz/FirebaseActivity.java
@@ -18,72 +18,12 @@ import com.firebase.client.ValueEventListener;
 public class FirebaseActivity extends ActionBarActivity {
 
     public static final String FIREBASE = "https://flickering-inferno-6814.firebaseio.com";
-    public static final String AUTH_TOKEN_EXTRA = "authToken";
-    public static final String AUTH_PROVIDER_EXTRA = "authProvider";
 
     /* A reference to the Firebase */
     protected Firebase mFirebaseRef;
 
     /* Data from the authenticated user */
     protected AuthData mAuthData;
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        /* If a user is currently authenticated, display a logout menu */
-        if (this.mAuthData != null) {
-            getMenuInflater().inflate(R.menu.main, menu);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_logout) {
-            logout();
-            return true;
-        }
-        else if (id == R.id.my_profile) {
-            displayProfile();
-            return true;
-        }
-        else if (id == R.id.my_groups) {
-            displayGroups();
-            return true;
-        }
-        else if (id == R.id.my_rides){
-            displayRides();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * Unauthenticate from Firebase and from providers where necessary.
-     */
-    private void logout() {
-        if (this.mAuthData != null) {
-            /* logout of Firebase */
-            mFirebaseRef.unauth();
-            /* Logout of any of the Frameworks. This step is optional, but ensures the user is not logged into
-             * Facebook/Google+ after logging out of Firebase. */
-            if (this.mAuthData.getProvider().equals("facebook")) {
-                /* Logout from Facebook */
-                LoginManager.getInstance().logOut();
-            }
-            /* Update authenticated user and show login buttons */
-            setAuthenticatedUser(null);
-
-            // Go back to login activity
-            Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
-            loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(loginIntent);
-            finish();
-            return;
-        }
-    }
 
     /**
      * Once a user is logged in, take the mAuthData provided from Firebase and "use" it.
@@ -98,8 +38,6 @@ public class FirebaseActivity extends ActionBarActivity {
             } else if (authData.getProvider().equals("anonymous")
                     || authData.getProvider().equals("password")) {
                 name = authData.getUid();
-            } else {
-                //Log.e(TAG, "Invalid provider: " + authData.getProvider());
             }
             if (name != null) {
                 Toast.makeText(getApplicationContext(), "Logged in as " + name + " (" + authData.getProvider() + ")",
@@ -126,33 +64,10 @@ public class FirebaseActivity extends ActionBarActivity {
             });
 
 
-        } else {
-            /* No authenticated user show all the login buttons */
         }
         this.mAuthData = authData;
         /* invalidate options menu to hide/show the logout button */
         supportInvalidateOptionsMenu();
-    }
-
-    /* displays the user's profile */
-    private void displayProfile() {
-        Intent profileIntent = new Intent(getApplicationContext(), ProfileActivity.class);
-        startActivity(profileIntent);
-        return;
-    }
-
-    /* displays the view from which the user can manage his/her ride*/
-    private void displayRides() {
-        Intent myRidesIntent = new Intent(getApplicationContext(), ManageMyRidesActivity.class);
-        startActivity(myRidesIntent);
-        return;
-    }
-
-    /* displays the view from which the user can manage his/her groups */
-    private void displayGroups() {
-        Intent myGroupsIntent = new Intent(getApplicationContext(), ManageMyGroupsActivity.class);
-        startActivity(myGroupsIntent);
-        return;
     }
 
 }
