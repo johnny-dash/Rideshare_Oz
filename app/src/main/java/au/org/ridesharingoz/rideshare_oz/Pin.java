68d9d3756a2f57be0a89f96d3625def8df932264
Daniel Chiuchiarelli <DChiuch@gmail.com>
2015-09-25 03:39:46 +1000

    More MapsActivity development

--- app/src/main/java/au/org/ridesharingoz/rideshare_oz/Pin.java
+++ app/src/main/java/au/org/ridesharingoz/rideshare_oz/Pin.java
@@ -1,11 +1,12 @@
 package au.org.ridesharingoz.rideshare_oz;
 
+import java.io.Serializable;
 import java.sql.Time;
 
 /**
  * Created by Johnny Mao on 15/9/23.
  */
-public class Pin {
+public class Pin implements Serializable {
     private int longtitude;
     private int latitude;
     private String address;
